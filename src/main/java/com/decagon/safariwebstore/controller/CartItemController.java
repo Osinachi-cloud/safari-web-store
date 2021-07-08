fa139c08d44ec015fbef9b497cff5f011ddc52b0
adenusidamilola5@gmail.com <adenusidamilola5@gmail.com>
2021-07-08 20:52:09 +0100

    added some endpoints in cart controller

--- src/main/java/com/decagon/safariwebstore/controller/CartItemController.java
+++ src/main/java/com/decagon/safariwebstore/controller/CartItemController.java
@@ -13,16 +13,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/products")
+@CrossOrigin
 public class CartItemController {
 
     private final CartItemService cartItemService;
     private final UserService userService;
     private final JWTUtil jwtUtil;
-
 
     @PostMapping("/add-to-cart")
     public ResponseEntity<Response> addItemsToCart(HttpServletRequest request,
@@ -30,11 +31,28 @@ public class CartItemController {
         String jwt = MethodUtils.parseJwt(request);
         String email = jwtUtil.extractUserName(jwt);
         User user = userService.findUserByEmail(email);
-
         Response response = cartItemService.addItemToCart(user, cartItemDTO);
         Integer status = response.getStatus();
+
         if (status.equals(201)) return new ResponseEntity<>(response, HttpStatus.CREATED);
         else if (status.equals(404)) return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+    @GetMapping("/cart-items")
+    @CrossOrigin
+    public ResponseEntity<List<CartItemDTO>> getCartItems(HttpServletRequest request) {
+
+        String jwt = MethodUtils.parseJwt(request);
+        String email = jwtUtil.extractUserName(jwt);
+        User user = userService.findUserByEmail(email);
+
+        return new ResponseEntity<>(cartItemService.getCartItems(user), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method=RequestMethod.DELETE)
+    @CrossOrigin
+    public ResponseEntity<Response> deleteCartItems(HttpServletRequest request, @PathVariable Long id) {
+        return cartItemService.deleteCartItemById(id);
     }
 }
