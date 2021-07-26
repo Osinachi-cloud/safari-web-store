2d4406e3b41b1d5f436dd6aca2d2c20a2112bef2
Francis Oyiogu <fcoyiogu@gmail.com>
2021-07-26 12:15:58 +0100

    Update ProductPage.java

--- src/main/java/com/decagon/safariwebstore/model/ProductPage.java
+++ src/main/java/com/decagon/safariwebstore/model/ProductPage.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Sort;
 @Data
 public class ProductPage {
     private int pageNumber = 0;
-    private int pageSize = 60;
+    private int pageSize = 100;
     private Sort.Direction sortDirection = Sort.Direction.ASC;
     private String sortBy = "name";
 }
